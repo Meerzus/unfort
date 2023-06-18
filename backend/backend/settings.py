7e451afc8e76804d9597210703f0d07150d99680
鬼鮫 <mmursaliev00@gmail.com>
2023-06-18 18:19:00 +0500

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+rjow@5&jqa&=ui-xx3by!gv5!d1#5+@86f9e%!jkhw(+^@bsi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['95.163.242.170']
+ALLOWED_HOSTS = ['95.163.242.170', '127.0.0.1']
 
 # Application definition
 
@@ -127,23 +127,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'unfort',
-        'USER': 'postgres',
-        'PASSWORD': '23LaMeerzus24',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'unfort',
+#         'USER': 'postgres',
+#         'PASSWORD': '23LaMeerzus24',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
