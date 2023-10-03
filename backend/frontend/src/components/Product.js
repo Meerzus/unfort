e62359b7c0ce2e56a0f496005f7609f15310e7c5
鬼鮫 <mmursaliev00@gmail.com>
2023-10-03 23:47:46 +0500

    updates

--- backend/frontend/src/components/Product.js
+++ backend/frontend/src/components/Product.js
@@ -27,48 +27,6 @@ function Product({product}) {
         img1[0].src = product.mainimg
     }
 
-    let likes = []
-
-    const liked = localStorage.getItem('likedItems')
-
-    const addToLike = () => {
-        let likedItem1 = document.getElementById(`${product._id}`)
-        let likedItem2 = likedItem1.getElementsByClassName('fa-heart')[0]
-
-        if (!(localStorage.likedItems.includes(product._id))) {
-            localStorage.likedItems = product._id + ', ' + localStorage.likedItems
-        } else if (localStorage.likedItems.includes(product._id)) {
-            localStorage.likedItems = localStorage.likedItems.replace(`${product._id}, `, '')
-        }
-
-        if (likedItem2.classList.contains('fa-regular')) {
-            likedItem1.classList.remove('liked')
-            likedItem2.classList.remove('fa-regular')
-            likedItem2.classList.add('fa-solid')
-        } else if (likedItem2.classList.contains('fa-solid')) {
-            likedItem1.classList.add('liked')
-            likedItem2.classList.remove('fa-solid')
-            likedItem2.classList.add('fa-regular')
-        }
-    }
-
-    const markAsLiked = () => {
-        let likedItem1 = document.getElementById(`${product._id}`)
-        let likedItem2 = likedItem1.getElementsByClassName('fa-heart')[0]
-
-        if (!(localStorage.likedItems.includes(product._id))) {
-            likedItem2.classList.remove('fa-solid')
-            likedItem2.classList.add('fa-regular')
-        } else if (localStorage.likedItems.includes(product._id)) {
-            likedItem2.classList.remove('fa-regular')
-            likedItem2.classList.add('fa-solid')
-        }
-    }
-
-    useEffect(() => {
-        markAsLiked()
-    }, [localStorage.likedItems])
-
     return (
         <motion.div
             animate={{opacity: 1, scale: 1}}
@@ -95,7 +53,6 @@ function Product({product}) {
                     onMouseOut={productImgOut}
                     className="rounded-3 product-img"/>
             </Link>
-            <i className="fa-regular fa-heart" id='like' onClick={addToLike}></i>
             <Card.Body>
                 <Link
                     onClick={linkHandler}
